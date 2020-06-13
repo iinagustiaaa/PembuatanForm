843c952e2d93097bfe428081e119e67d9594807b
iinagustiaaa <iinagustiaaa@gmail.com>
2020-06-13 08:33:39 +0700

    PembuatanForm

---
+++ mhs.php
@@ -0,0 +1,46 @@
+<html>
+<style>
+  table {
+    border-collapse: collapse;
+  }
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+<body><table style="width:50%">
+
+  <tr>
+    <td>NIM</td>
+    <td><?php echo $_POST["NIM"]; ?></td> 
+     </tr>
+  <tr>
+    <td>Nama</td>
+    <td><?php echo $_POST["NAMA"]; ?></td>
+    </tr>
+  <tr>
+    <td>Jenis Kelamin</td>
+    <td><?php echo $_POST["KELAMIN"]; ?></td>
+    </tr>
+  <tr>
+    <td>Alamat</td>
+    <td><?php echo $_POST["ALAMAT"]; ?></td>
+     </tr>
+  <tr>
+    <td>Program Studi</td>
+    <td><?php echo $_POST["PRODI"]; ?></td>
+     </tr>
+  <tr>
+  <td>Bahasa Pemrograman yang dikuasai </td>
+    <td><?php if (isset($_POST['check'])) {
+
+        $check=$_POST['check'];
+    echo "";
+        for ($i=0; $i < count($check) ; $i++){
+            echo $check[$i]."<br>";
+        }
+    }
+  ?></td>
+     </tr>
+</table>
+</body>
+</html>
